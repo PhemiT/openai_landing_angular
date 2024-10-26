d3d0f1b2c33a9d2ac18be37a4fb85914b30735a4
Olufemi Olubukunmi <phemiwrites@gmail.com>
2024-10-26 15:27:11 +0100

    deploy fix

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { faChevronDown, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +7,6 @@ import { faChevronDown, faBars, faTimes } from '@fortawesome/free-solid-svg-icon
 })
 export class NavbarComponent {
   toggleMenu: boolean = false;
-  menuIcon = faBars;
-  closeIcon = faTimes;
   
   toggleMenuState(): void {
     this.toggleMenu = !this.toggleMenu;
